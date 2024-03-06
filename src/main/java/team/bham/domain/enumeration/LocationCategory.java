5b63d7d166821e78ca5b53bb865db7b9db1ffae9
Shivamkumar Patel <smp237@student.bham.ac.uk>
2024-03-06 05:38:29 +0000

    Revert "Latest Repo"
    This reverts commit 227290e64dd067cf2be2a23fe564a629e20c5ee2

--- src/main/java/team/bham/domain/enumeration/LocationCategory.java
+++
@@ -1,9 +0,0 @@
-package team.bham.domain.enumeration;
-
-/**
- * The LocationCategory enumeration.
- */
-public enum LocationCategory {
-    INPERSON,
-    REMOTE,
-}
--- src/main/webapp/app/entities/social-feed/social-feed.model.ts
+++
@@ -1,8 +0,0 @@
-import dayjs from 'dayjs/esm';
-
-export interface ISocialFeed {
-  id: number;
-  lastUpdated?: dayjs.Dayjs | null;
-}
-
-export type NewSocialFeed = Omit<ISocialFeed, 'id'> & { id: null };
