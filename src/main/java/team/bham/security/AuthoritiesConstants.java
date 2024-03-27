292d9d5376bfcb6aac20c01d1118ace916fc65f2
Shivamkumar Patel <smp237@student.bham.ac.uk>
2024-03-27 15:04:03 +0000

    Revert "Add Charity_Admin role"
    This reverts commit 1c302e0e9b73b700ca44a483bec6d2e4e55a798f

--- src/main/java/team/bham/security/AuthoritiesConstants.java
+++ src/main/java/team/bham/security/AuthoritiesConstants.java
@@ -11,7 +11,5 @@ public final class AuthoritiesConstants {
 
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
-    public static final String CHARITYADMIN = "ROLE_CHARITY_ADMIN";
-
     private AuthoritiesConstants() {}
 }
